ede702dfd86bcbfea9d5f0904a0d82b0a8c77713
dvnnywrld <ibedaniel207@gmail.com>
2025-04-29 12:09:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('toggle-theme');
+    toggleBtn.addEventListener('click', () => {
+        document.body.classList.toggle('dark');
+    });
+});
